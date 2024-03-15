efeb2226ea65d33cfe4f134cb4ebede6e629d408
anchalkushwaha <anchal.kushwaha@thoughtworks.com>
2024-03-15 19:13:45 +0530

    moved files to src folder

--- GalleryApp/App.tsx
+++ GalleryApp/App.tsx
@@ -1,63 +1,58 @@
-
-import React, {useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { Button, View } from 'react-native';
 import {
-  Button,
-  Image,
-  Text,
-  TouchableOpacity,
-  View
-} from 'react-native';
-import ListScreen from './View/ListScreen';
-import ImageDetailScreen from './View/ImageDetailScreen';
-import CounterScreen from './View/CounterScreen';
-import ColorScreen from './View/ColorScreen';
-import {ImageLibraryOptions, launchImageLibrary} from 'react-native-image-picker';
+  ImageLibraryOptions,
+  launchImageLibrary,
+} from 'react-native-image-picker';
+import ImageDetailScreen from './src/View/ImageDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
-const App = () => {
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen
+        name="ImageDetailsScreen"
+        component={ImageDetailScreen}
+        options={{
+          headerRight: headerRightButton,
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name = 'Home' component = {Home}/>
-        <Stack.Screen name = 'ImageDetailScreen' component = {ImageDetailScreen} options={(navigation) =>({
-          
-        headerRight: () => {
-          return <Button title='ADD' onPress={openImagePicker}></Button>
-      }})}/>
- 
-      </Stack.Navigator>
-    </NavigationContainer>
-   
-  );
-}
+const headerRightButton = () => (
+  <Button title="ADD" onPress={openImagePicker} />
+);
 
 const openImagePicker = () => {
   const options: ImageLibraryOptions = {
     mediaType: 'photo',
     includeBase64: false,
-    
   };
-  launchImageLibrary(options, (response) => {
+  launchImageLibrary(options, response => {
     if (response.didCancel) {
       console.log('User cancelled image picker');
-    } else if (response.errorCode != null) {
-      console.log('Image picker error: ', response.errorMessage);
-    } else {
-      console.warn(response.assets);
+      return;
     }
-  })
-
+    if (response.errorCode != null) {
+      console.log('Image picker error: ', response.errorMessage);
+      return;
+    }
+  });
 };
 
-const Home = (props: any) => {
-  return  <View>
-    <Button title='go to Image screen' onPress={() => props.navigation.navigate('ImageDetailScreen')}></Button>
-    </View>
-}
-
+const Home = (props: any) => (
+  <View>
+    <Button
+      title="go to Image screen"
+      onPress={() => props.navigation.navigate('ImageDetailsScreen')}
+    />
+  </View>
+);
 
 export default App;
---
+++ GalleryApp/src/View/ImageDetailScreen.js
@@ -0,0 +1,104 @@
+import React, { useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  ImageBackground,
+} from 'react-native';
+import { launchImageLibrary } from 'react-native-image-picker';
+
+const ImageDetailScreen = ({ navigation }) => {
+  const users1 = [
+    {
+      title: 'mountain',
+      imageSource: require('../images/beach.jpg'),
+      imageScore: 9,
+    },
+    {
+      title: 'sea',
+      imageSource: require('../images/beach.jpg'),
+      imageScore: 7,
+    },
+    {
+      title: 'tiger',
+      imageSource: require('../images/forest.jpg'),
+      imageScore: 6,
+    },
+    {
+      title: 'mountain',
+      imageSource: require('../images/beach.jpg'),
+      imageScore: 9,
+    },
+    {
+      title: 'sea',
+      imageSource: require('../images/beach.jpg'),
+      imageScore: 7,
+    },
+  ];
+
+  // React.useEffect(() => {
+  //     navigation.setOptions({headerRight: () => {
+  //         <Button title='ADD' onPress={
+  //             openImagePicker
+  //         }/>
+  //     }})
+  // },[navigation]);
+
+  return (
+    <ScrollView>
+      <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap' }}>
+        {users1.map(item => (
+          <View style={styles.viewStyle}>
+            <ImageBackground
+              style={styles.imageStyle}
+              source={item.imageSource}
+              resizeMode="cover">
+              <TouchableOpacity style={styles.buttonStyle}>
+                <Text>add</Text>
+              </TouchableOpacity>
+            </ImageBackground>
+            <Text style={styles.textStyle}>{item.title}</Text>
+            <Text style={styles.textStyle}>ImageScore {item.imageScore}</Text>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: { marginBottom: 10 },
+  viewStyle: { alignItems: 'center', padding: 16 },
+  imageStyle: {
+    flex: 1,
+    height: 100,
+    width: 100,
+  },
+  buttonStyle: {
+    backgroundColor: 'grey',
+    position: 'absolute',
+    right: 0,
+  },
+});
+
+// const openImagePicker = () => {
+//     const options = {
+//       mediaType: 'photo',
+//       includeBase64: false,
+
+//     };
+//     launchImageLibrary(options, (response) => {
+//       if (response.didCancel) {
+//         console.log('User cancelled image picker');
+//       } else if (response.errorCode != null) {
+//         console.log('Image picker error: ', response.errorMessage);
+//       } else {
+//         console.warn(response.assets);
+//       }
+//     })
+
+//   };
+
+export default ImageDetailScreen;
